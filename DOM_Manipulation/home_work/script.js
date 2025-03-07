835bb130f918e717d89ec4c025f130c6b3845d01
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-07 14:23:09 +0500

    DOM Manipulation

--- DOM_Manipulation/home_work/script.js
+++ DOM_Manipulation/home_work/script.js
@@ -1,13 +1,13 @@
 let webdevelopmentData = [
-    { name: "wafi", isGraduate: true },
-    { name: "zain", isGraduate: true },
-    { name: "Shan", isGraduate: false },
-    { name: "saliha", isGraduate: true },
-    { name: "Daniyal", isGraduate: false },
-  ];
-  let GraduateStd = document.getElementById("stdData");
+  { name: "wafi", isGraduate: true },
+  { name: "zain", isGraduate: true },
+  { name: "Shan", isGraduate: false },
+  { name: "saliha", isGraduate: true },
+  { name: "Daniyal", isGraduate: false },
+];
+let GraduateStd = document.getElementById("stdData");
 
-let filteredGraduate =  webdevelopmentData
+let filteredGraduate = webdevelopmentData
   .filter((stds) => {
     return stds.isGraduate === true;
   })
@@ -18,4 +18,4 @@ let filteredGraduate =  webdevelopmentData
 
 console.log(filteredGraduate);
 
-GraduateStd.innerHTML = filteredGraduate; 
+GraduateStd.innerHTML = filteredGraduate;
