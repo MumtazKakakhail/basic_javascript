880e8f770068adfac26a60333197c0acd317cf2e
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-07 14:22:02 +0500

    DOM Maniplation

---
+++ DOM_Manipulation/home_work/script.js
@@ -0,0 +1,21 @@
+let webdevelopmentData = [
+    { name: "wafi", isGraduate: true },
+    { name: "zain", isGraduate: true },
+    { name: "Shan", isGraduate: false },
+    { name: "saliha", isGraduate: true },
+    { name: "Daniyal", isGraduate: false },
+  ];
+  let GraduateStd = document.getElementById("stdData");
+
+let filteredGraduate =  webdevelopmentData
+  .filter((stds) => {
+    return stds.isGraduate === true;
+  })
+  .map((grdstd) => {
+    return `<li>${grdstd.name}</li>`;
+  })
+  .join("");
+
+console.log(filteredGraduate);
+
+GraduateStd.innerHTML = filteredGraduate; 
