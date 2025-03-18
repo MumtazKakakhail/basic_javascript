592bada51bef608a32802925703affce93a4c157
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-18 23:11:54 +0500

    Functions in Javascript

--- Functions/script.js
+++ Functions/script.js
@@ -28,3 +28,39 @@ function firstName(name) {
     console.log(name);
 }
 console.log(firstName("saima")); 
+
+ // Anonymous Functions
+
+ let name1 = function () {
+   console.log("Anonymous Functions");
+ };
+ name1();
+ //Arrow Functions =>
+
+ const lastName = (last_name) => {
+   console.log(last_name, "last name");
+ };
+ lastName("AKhtar");
+
+ // Callbacks and Higher Order Functions
+
+ function example(callBacks) {
+   // let callBack = example2()
+   callBacks();
+ }
+ function example2() {
+   console.log("hi there!");
+ }
+ example(example2);
+
+ function printName(callBack) {
+   let name3 = "saima";
+   callBack(name3); // callback = myName()   
+      }
+ function myName(name3) {
+   console.log("hi" , name3);
+ }
+ printName(myName);
+
+
+ // sumOfTwoNumbers
